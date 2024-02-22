dcc12adff138084d286b5161b1742ea4537a38a0
Luigi Balestrucci <card3001@outlook.it>
2024-02-22 19:02:44 +0100

    Exercise Navbar responsive completed

---
+++ script.js
@@ -0,0 +1,15 @@
+//Select element in HTML:
+let footerPage = document.querySelector('.footer-page');
+//Var on date copiryght:
+let date = new Date();
+let year = date.getFullYear();
+footerPage.innerHTML = `<span>Copyright © ${year} : <a href='https://www.develhope.co/it/sign-up/'><strong>develhope.co</strong></a></span>`;
+
+//Var to select icon hamburger:
+let item = document.querySelector('.icon-hamburger');
+
+//Event to click icon add class on body:
+item.addEventListener("click", function() {
+  document.body.classList.toggle('toggle-active');
+});
+
